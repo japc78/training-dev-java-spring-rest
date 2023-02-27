56d593facd9d3245436e0b67e91f9f355ff2b1d6
japc <japc.grafico@gmail.com>
2023-02-27 22:13:53 +0100

    Add - Basic caching to roles

--- src/main/java/com/japcdev/userapp/services/RoleService.java
+++ src/main/java/com/japcdev/userapp/services/RoleService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -12,20 +14,25 @@ import com.japcdev.userapp.entities.Role;
 import com.japcdev.userapp.repositories.RoleRepository;
 
 @Service
-public class RoleService {
+public class RoleService extends appService{
 
   @Autowired
   private RoleRepository repository;
 
+  @Cacheable("roles")
   public List<Role> getRoles() {
+    logger.info("Getting roles from cache");
     return repository.findAll();
   }
 
+  @CacheEvict(value = "roles" , allEntries = true)
   public Role createRole(Role role) {
     return repository.save(role);
   }
 
+  @CacheEvict(value = "roles" , allEntries = true)
   public Role updateRole(Integer roleId, Role role) {
+    logger.info("Update roles to cache");
     Optional<Role> findById = repository.findById(roleId);
 
     if (!findById.isPresent()) {
@@ -35,14 +42,16 @@ public class RoleService {
     return repository.save(role);
   }
 
-    public void deleteRole(Integer roleId) {
-      Optional<Role> findById = repository.findById(roleId);
+  @CacheEvict(value = "roles" , allEntries = true)
+  public void deleteRole(Integer roleId) {
+    logger.info("Delete role to cache");
+    Optional<Role> findById = repository.findById(roleId);
 
-      if (!findById.isPresent()) {
-        throw new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("Role id %d doesn't exists", roleId));
-      }
-
-      repository.delete(findById.get());
+    if (!findById.isPresent()) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("Role id %d doesn't exists", roleId));
     }
+
+    repository.delete(findById.get());
+  }
 
 }
